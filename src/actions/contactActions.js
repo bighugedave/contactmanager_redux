ad381b50753abd029dfbf3c6eeebda69efbf4566
David Ayres <superdavidayres@fastmail.com>
2019-11-21 08:24:28 -0500

    initial commit

---
+++ src/actions/contactActions.js
@@ -0,0 +1,25 @@
+import { GET_CONTACTS, ADD_CONTACT, DELETE_CONTACT } from "./types";
+import axios from "axios";
+
+export const getContacts = () => async dispatch => {
+  const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+  dispatch({
+    type: GET_CONTACTS,
+    payload: res.data
+  });
+};
+
+export const deleteContact = id => async dispatch => {
+  await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+  dispatch({
+    type: DELETE_CONTACT,
+    payload: id
+  });
+};
+
+export const addContact = contact => {
+  return {
+    type: ADD_CONTACT,
+    payload: contact
+  };
+};
